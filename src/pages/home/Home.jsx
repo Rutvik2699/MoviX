b2153c4771e4449c0960bd97e46ee1d359ab7d5a
Rutvik2699 <rutvikbhoj2611@gmail.com>
2023-11-08 18:34:15 -0500

    Deployed website

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@
 
 import "./style.scss";
 
-import HeroBanner from "./heroBanner/HeroBanner";
+import HeroBanner from "./HeroBanner/HeroBanner";
 import Trending from "./trending/Trending";
 import Popular from "./popular/Popular";
 import TopRated from "./topRated/TopRated";
